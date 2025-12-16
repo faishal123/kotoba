bb2578f9950cfd9d991110fca100b33c144f1c9d
Faishal <faisal@tantowi.com>
2025-12-16 12:52:06 +0700

    fix: build error

--- src/app/quiz/random-number/page.tsx
+++ src/app/quiz/random-number/page.tsx
@@ -1,72 +1,19 @@
 "use client";
-import { ChooseQuestionCountPage } from "@/components/Molecules/ChooseQuestionCountPage/ChooseQuestionCountPage";
-import { HeaderBackButton } from "@/components/Molecules/Header/BackButton";
-import { QuizScreen } from "@/components/Page/QuizScreen/QuizScreen";
-import { AnswerType, QuestionType } from "@/constant/types";
-import { generateRandomNumber, randomizeArray } from "@/utils/serverUtils";
-import { translateNumberToJapanese } from "japanese-number-translator";
-import { useSearchParams } from "next/navigation";
+
+import RandomNumberQuiz from "@/components/Page/RandomNumberQuiz/RandomNumberQuiz";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-  const questionCountParam = searchParams.get("questionCount");
-  const questionCount = Number(questionCountParam || 0);
-
-  if (!questionCountParam) {
-    return (
-      <div className="w-[100svw] h-[100svh] flex flex-col p-10">
-        <HeaderBackButton />
-        <ChooseQuestionCountPage />
-      </div>
-    );
-  }
-
-  const questions: QuestionType[] = Array.from(Array(questionCount)).reduce<
-    QuestionType[]
-  >((a) => {
-    const correctAnswer = generateRandomNumber(1, 100000);
-    const wrongAnswers: number[] = [];
-    while (wrongAnswers.filter((a) => a !== correctAnswer).length < 2) {
-      wrongAnswers.push(generateRandomNumber(1, 100000));
-    }
-
-    const answers: AnswerType[] = randomizeArray([
-      {
-        answer: `${correctAnswer}`,
-        isCorrect: true,
-      },
-      {
-        answer: `${wrongAnswers?.[0]}`,
-        isCorrect: false,
-      },
-      {
-        answer: `${wrongAnswers?.[1]}`,
-        isCorrect: false,
-      },
-    ]);
-
-    const correctAnswerTranslated = translateNumberToJapanese(
-      `${correctAnswer}`
-    );
-    return [
-      ...a,
-      {
-        question: correctAnswerTranslated?.kana,
-        answers,
-      },
-    ];
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    setRendered(true);
   }, []);
-
+  if (!rendered) {
+    return null;
+  }
   return (
-    <QuizScreen
-      questionClassName="text-5xl xs:text-8xl text-center"
-      answerClassName="text-xl xs:text-3xl"
-      answersContainerClassName="flex-col xs:flex-row"
-      containerClassName="pt-[168px] pb-[140px] px-10 xs:pt-0 xs:pb-0"
-      homeUrl="/"
-      levelName={"Random Numbers"}
-      questions={questions}
-      answerMethod="input"
-    />
+    <Suspense>
+      <RandomNumberQuiz />
+    </Suspense>
   );
 }
