f6cb436d44697a97d37d24cb285a822974fde6f8
Faishal <faisal@tantowi.com>
2026-01-01 02:37:24 +0700

    fix: add quiz search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Card } from "@/components/Atoms/Card/Card";
 import { Header } from "@/components/Molecules/Header/Header";
 import { characterChoices } from "@/constant/characterChoices";
@@ -7,11 +8,20 @@ import Link from "next/link";
 import { MixQuizSelector } from "@/components/Molecules/MixQuizSelector/MixQuizSelector";
 import { useAvailableQuizList } from "@/services/available-quiz-list/useAvailableQuizList";
 import { Spinner } from "@/components/ui/spinner";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
   const { data, isPending } = useAvailableQuizList();
 
-  const quizWithQuestions = data?.filter((quiz) => quiz.question_count > 0);
+  const quizWithQuestions = data?.filter(
+    (quiz) =>
+      quiz.question_count > 0 &&
+      (quiz.quiz_name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        quiz.description.toLowerCase().includes(searchValue.toLowerCase()))
+  );
+
+  const trimmedSearchValue = searchValue.trim().toLowerCase();
 
   return (
     <>
@@ -20,18 +30,33 @@ export default function Home() {
         customTitle="KOTOBA"
         customCharacter="言葉"
       />
-      <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {characterChoices.map((choice) => (
-          <Link key={choice.href} href={choice.href}>
-            <Card className="w-full h-full p-5 flex gap-5 items-center">
-              {/* <div className="text-4xl font-bold">{choice.character}</div> */}
-              <div>
-                <div className="text-lg font-bold">{choice.label}</div>
-                <div className="text-lg">{choice.description}</div>
-              </div>
-            </Card>
-          </Link>
-        ))}
+      <div className="px-10 pt-5 pb-0">
+        <Input
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+          placeholder="Search Quiz"
+        />
+      </div>
+      <div className="p-10 pt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {characterChoices
+          .filter(
+            (choice) =>
+              choice.label.toLowerCase().includes(trimmedSearchValue) ||
+              choice.description.toLowerCase().includes(trimmedSearchValue)
+          )
+          .map((choice) => (
+            <Link key={choice.href} href={choice.href}>
+              <Card className="w-full h-full p-5 flex gap-5 items-center">
+                {/* <div className="text-4xl font-bold">{choice.character}</div> */}
+                <div>
+                  <div className="text-lg font-bold">{choice.label}</div>
+                  <div className="text-lg">{choice.description}</div>
+                </div>
+              </Card>
+            </Link>
+          ))}
         {quizWithQuestions && <MixQuizSelector quizList={quizWithQuestions} />}
         {isPending && (
           <Card className="w-full h-full p-5 flex gap-5 items-center justify-center">
